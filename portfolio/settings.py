24f1b49bb139b3c398125fe824e35bbae3dfdc7f
Manish Sharma <mrsharma98@gmail.com>
2020-03-18 01:09:55 +0530

    Jobs app up and runing

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -9,6 +9,11 @@ https://docs.djangoproject.com/en/2.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.2/ref/settings/
 """
+"""
+username:- manish
+password:- 123456
+"""
+
 
 import os
 
@@ -31,6 +36,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jobs.apps.JobsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +81,12 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'portfoliodb',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -118,3 +128,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# For saving media files
+# this will automatically make a directory.
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# media/images/filename
+
+
+MEDIA_URL = '/media/'
+# if someone wants to get the media that we have uploaded
+# then they will go to this path.
+# eg localhost/admin/media/images/filename when we open using browser admin
+# we can put anything instead of media
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -16,6 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from django.conf import settings
+from django.conf.urls.static import static
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+# whenever we upload images the we want to have media url and media root
+# so that we can view images using admin page.
