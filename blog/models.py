ed875787501a18dbdf2b812e25d0eb2e90d0e7ab
Manish Sharma <mrsharma98@gmail.com>
2020-03-18 01:51:06 +0530

    started templates

--- blog/models.py
+++ blog/models.py
@@ -1,1 +1,9 @@
 from django.db import models
+
+
+class Blog(models.Model):
+
+    title = models.CharField(max_length=255)
+    pub_date = models.DateTimeField()
+    body = models.TextField()
+    image = models.ImageField(upload_to='images/')
--- portfolio/urls.py
+++ portfolio/urls.py
@@ -18,9 +18,12 @@ from django.urls import path
 
 from django.conf import settings
 from django.conf.urls.static import static
+import jobs.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', jobs.views.home, name='home'),
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 # whenever we upload images the we want to have media url and media root
